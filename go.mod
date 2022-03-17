f1579348a09b5691d6df249bc8b1e8a38754c230
Lorenz Bauer <i@lmb.io>
2022-03-17 09:34:39 +0000

    exec init in VM

---
+++ go.mod
@@ -0,0 +1,14 @@
+module lmb.io/vmrun
+
+go 1.17
+
+require (
+	github.com/dustin/go-humanize v1.0.0 // indirect
+	github.com/google/goexpect v0.0.0-20191001010744-5b6988669ffa // indirect
+	github.com/google/goterm v0.0.0-20190703233501-fc88cf888a3f // indirect
+	github.com/pierrec/lz4/v4 v4.1.11 // indirect
+	github.com/u-root/u-root v0.8.0 // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+	golang.org/x/sys v0.0.0-20210820121016-41cdb8703e55 // indirect
+	google.golang.org/grpc v1.29.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+
+	"golang.org/x/sys/unix"
+)
+
+func main() {
+	var err error
+	if os.Getpid() == 1 {
+		err = minimalInit(realSyscaller{}, os.Args[1:])
+	} else {
+		err = run(os.Args[1:])
+	}
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error:", err)
+		os.Exit(1)
+	}
+}
+
+func run(args []string) error {
+	fs := flag.NewFlagSet("vmrun", flag.ContinueOnError)
+	kernel := fs.String("vm.kernel", "", "`path` to the Linux image")
+	if err := fs.Parse(args); errors.Is(err, flag.ErrHelp) {
+		return nil
+	} else if err != nil {
+		return err
+	}
+
+	initPath, err := findExecutable()
+	if err != nil {
+		return err
+	}
+
+	vm, err := execInVM(&command{
+		Kernel: *kernel,
+		Path:   initPath,
+		Args:   fs.Args(),
+		Stdin:  os.Stdin,
+		Stdout: os.Stdout,
+		Stderr: os.Stderr,
+	})
+	if err != nil {
+		return err
+	}
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	go func() {
+		<-ctx.Done()
+		vm.Process.Kill()
+	}()
+
+	if err := vm.Wait(); err != nil {
+		return fmt.Errorf("qemu: %w", err)
+	}
+
+	return nil
+}
+
+func findExecutable() (string, error) {
+	// https://man7.org/linux/man-pages/man5/proc.5.html
+	buf := make([]byte, unix.NAME_MAX)
+	n, err := unix.Readlink("/proc/self/exe", buf)
+	if err != nil {
+		return "", fmt.Errorf("readlink /proc/self/exe: %w", err)
+	}
+
+	if n == unix.NAME_MAX {
+		return "", fmt.Errorf("readlink returned truncated name")
+	}
+
+	path := unix.ByteSliceToString(buf)
+	if _, err := os.Stat(path); err != nil {
+		// Make sure the symlink doesn't reference a deleted file.
+		return "", err
+	}
+
+	return path, nil
+}
